b45b76fd1a18b3594d072d47fed86070f9969a7e
Micheal LeBlanc <Mikeleblanc8489@gmail.com>
2023-03-12 19:17:57 -0400

    Tic tac toe via command line

---
+++ tictactoe.py
@@ -0,0 +1,235 @@
+#Nested list as a board
+board = [['-', '-', '-'],
+         ['-', '-', '-'],
+         ['-', '-', '-'] ]
+
+#Display board
+print(board[0])
+print(board[1])
+print(board[2])
+
+#X User input
+col = int(input("X player, select a column 1-3: "))
+row = int(input("X player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+if (board[row][col] == '-'):
+    board[row][col] = 'X'
+else:
+    print("This spot on the board is already taken")
+
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+
+#O user input
+col = int(input("O player, select a column 1-3: "))
+row = int(input("O player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+#Input validation
+if (board[row][col] == '-'):
+    board[row][col] = 'O'
+else:
+    print("This spot on the board is already taken")
+
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+
+#X User input
+col = int(input("X player, select a column 1-3: "))
+row = int(input("X player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+if (board[row][col] == '-'):
+    board[row][col] = 'X'
+else:
+    print("This spot on the board is already taken")
+    
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+
+#O user input
+col = int(input("O player, select a column 1-3: "))
+row = int(input("O player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+#Input validation
+if (board[row][col] == '-'):
+    board[row][col] = 'O'
+else:
+    print("This spot on the board is already taken")
+
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+#X User input
+col = int(input("X player, select a column 1-3: "))
+row = int(input("X player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+if (board[row][col] == '-'):
+    board[row][col] = 'X'
+else:
+    print("This spot on the board is already taken")
+    
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+
+#O user input
+col = int(input("O player, select a column 1-3: "))
+row = int(input("O player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+#Input validation
+if (board[row][col] == '-'):
+    board[row][col] = 'O'
+else:
+    print("This spot on the board is already taken")
+
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+#X User input
+col = int(input("X player, select a column 1-3: "))
+row = int(input("X player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+if (board[row][col] == '-'):
+    board[row][col] = 'X'
+else:
+    print("This spot on the board is already taken")
+    
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+
+#O user input
+col = int(input("O player, select a column 1-3: "))
+row = int(input("O player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+#Input validation
+if (board[row][col] == '-'):
+    board[row][col] = 'O'
+else:
+    print("This spot on the board is already taken")
+
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+#X User input
+col = int(input("X player, select a column 1-3: "))
+row = int(input("X player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+if (board[row][col] == '-'):
+    board[row][col] = 'X'
+else:
+    print("This spot on the board is already taken")
+    
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+
+#O user input
+col = int(input("O player, select a column 1-3: "))
+row = int(input("O player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+#Input validation
+if (board[row][col] == '-'):
+    board[row][col] = 'O'
+else:
+    print("This spot on the board is already taken")
+
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+#X User input
+col = int(input("X player, select a column 1-3: "))
+row = int(input("X player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+if (board[row][col] == '-'):
+    board[row][col] = 'X'
+else:
+    print("This spot on the board is already taken")
+    
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+
+#O user input
+col = int(input("O player, select a column 1-3: "))
+row = int(input("O player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+#Input validation
+if (board[row][col] == '-'):
+    board[row][col] = 'O'
+else:
+    print("This spot on the board is already taken")
+
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+#X User input
+col = int(input("X player, select a column 1-3: "))
+row = int(input("X player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+if (board[row][col] == '-'):
+    board[row][col] = 'X'
+else:
+    print("This spot on the board is already taken")
+    
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
+
+#O user input
+col = int(input("O player, select a column 1-3: "))
+row = int(input("O player, select a row 1-3: "))
+col -= 1
+row -= 1
+
+#Input validation
+if (board[row][col] == '-'):
+    board[row][col] = 'O'
+else:
+    print("This spot on the board is already taken")
+
+#Update board
+print(board[0])
+print(board[1])
+print(board[2])
